4a3f31879fd5ed3f192eb3921c29422b6f0094f5
Mark Sibly <marksibly@gmail.com>
2024-03-14 12:37:44 +1300

    Update nodejs

---
+++ libsgd/python/helloworld.py
@@ -0,0 +1,9 @@
+import libsgd as sgd
+
+sgd.CreateWindow(640, 480, "Hello World!", 0)
+sgd.CreateScene()
+sgd.SetSceneClearColor(1,.5,0,1)
+
+while(not sgd.PollEvents()):
+    sgd.RenderScene()
+    sgd.Present()
