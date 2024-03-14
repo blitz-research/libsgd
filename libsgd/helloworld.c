b9a8b99b2fc4c21c5bbb232aa3fde27cf7d80b49
Mark Sibly <marksibly@gmail.com>
2024-03-15 09:10:50 +1300

    Update

---
+++ libsgd/helloworld.c
@@ -0,0 +1,18 @@
+//
+// This can be built with something like:
+//
+// gcc -I../../include -L. -DSGD_DYNAMIC=1 -o helloworld helloworld.c -lsgd_dynamic
+
+#include <sgd/sgd.h>
+
+int main() {
+    
+    sgd_CreateWindow(640, 480, "Hello World!", 0);
+    sgd_CreateScene();
+    sgd_SetSceneClearColor(1,.5,0,1);
+    
+    while(!sgd_PollEvents()) {
+        sgd_RenderScene();
+        sgd_Present();
+    }
+}
