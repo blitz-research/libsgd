ed05764e80b53ec9a4a6a1f6495c539d0dfa7d0f
Mark Sibly <marksibly@gmail.com>
2024-08-06 10:19:10 +1200

    Update

--- libsgd/python/pypi/build.sh
+++ libsgd/python/pypi/build.sh
@@ -1,6 +1,8 @@
+
 rm -rf dist
 rm -rf build
-cp ../sgd.py libsgd
-cp ../_sgd.pyd libsgd
-python -m build
 
+cp ../../../cmake-build-windows-x64-release/LIBSGD_INSTALL/python/sgd.py libsgd
+cp ../../../cmake-build-windows-x64-release/LIBSGD_INSTALL/python/_sgd.pyd libsgd
+
+python -m build
