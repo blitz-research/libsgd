3051af7c61ea0f74bad1a7480b0a0fbabfe712b4
Mark Sibly <marksibly@gmail.com>
2024-09-02 21:34:51 +1200

    Add dawn metal test

---
+++ dawn/tests/metal_tests.cpp
@@ -0,0 +1,95 @@
+#include <dawn/exports.h>
+
+#include <cassert>
+
+#include <GLFW/glfw3.h>
+
+#if SGD_OS_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32 1
+#define GLFW_EXPOSE_NATIVE_WGL 1
+#define WIN32_LEAN_AND_MEAN
+#include <GLFW/glfw3native.h>
+#undef min
+#undef max
+#elif SGD_OS_LINUX
+#define GLFW_EXPOSE_NATIVE_X11 1
+#define GLFW_EXPOSE_NATIVE_GLX 1
+#include <GLFW/glfw3native.h>
+#undef Success
+#undef Always
+#undef None
+#endif
+
+using namespace sgd;
+
+GLFWwindow* window;
+
+wgpu::Device device;
+wgpu::Surface surface;
+wgpu::Texture renderTarget;
+
+void render() {
+
+	drawHelloTriangle(device, renderTarget);
+
+	struct wgpu::SurfaceTexture surfaceTexture {};
+	surface.GetCurrentTexture(&surfaceTexture);
+	assert(surfaceTexture.status == wgpu::SurfaceGetCurrentTextureStatus::Success);
+
+	copyTexture(device, renderTarget, surfaceTexture.texture);
+
+	surface.Present();
+}
+
+int main() {
+
+	uint32_t width = 640;
+	uint32_t height = 480;
+
+	if (!glfwInit()) SGD_ABORT();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+#if SGD_OS_MACOS
+	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
+#endif
+
+	window = glfwCreateWindow((int)width, (int)height, "Dawn test", nullptr, nullptr);
+
+	device = createWGPUDevice({});
+	surface = createWGPUSurface(device, window);
+
+	static auto configureSurface = [=](CVec2u size) {
+		wgpu::SurfaceConfiguration config{};
+		config.device = device;
+		config.width = width;
+		config.height = height;
+		config.format = surface.GetPreferredFormat(device.GetAdapter());
+		config.usage = wgpu::TextureUsage::RenderAttachment;
+		config.presentMode = wgpu::PresentMode::Fifo;
+		config.viewFormatCount = 0;
+		config.viewFormats = nullptr;
+		config.alphaMode = wgpu::CompositeAlphaMode::Opaque;
+		surface.Configure(&config);
+	};
+	configureSurface(Vec2u(width,height));
+
+	static auto resizeRenderTarget = [=](CVec2u size) {
+		wgpu::TextureDescriptor desc{};
+		desc.size = {size.x,size.y,1};
+		desc.format = wgpu::TextureFormat::RGBA16Float;
+		desc.usage = wgpu::TextureUsage::RenderAttachment | wgpu::TextureUsage::TextureBinding;
+		renderTarget = device.CreateTexture(&desc);
+	};
+	resizeRenderTarget(Vec2u(width,height));
+
+	glfwSetWindowSizeCallback(window, [](GLFWwindow*, int x, int y) {
+		configureSurface(Vec2u(x,y));
+		resizeRenderTarget(Vec2u(x,y));
+	});
+
+	for (;;) {
+		glfwPollEvents();
+		if (glfwWindowShouldClose(window)) std::exit(0);
+
+		render();
+	}
+}
