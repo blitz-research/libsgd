9757d3dc77b0e9f3c2880d3115d7568dc7a9513b
marksibly <marksibly@zohomail.com>
2024-09-04 10:50:05 +1200

    Add dawn metal tests

--- dawn/tests/metal_tests.cpp
+++ dawn/tests/metal_tests.cpp
@@ -1,95 +1,231 @@
-#include <dawn/exports.h>
-
 #include <cassert>
+#include <cstdlib>
 
 #include <GLFW/glfw3.h>
-
-#if SGD_OS_WINDOWS
 #define GLFW_EXPOSE_NATIVE_WIN32 1
-#define GLFW_EXPOSE_NATIVE_WGL 1
-#define WIN32_LEAN_AND_MEAN
 #include <GLFW/glfw3native.h>
-#undef min
-#undef max
-#elif SGD_OS_LINUX
-#define GLFW_EXPOSE_NATIVE_X11 1
-#define GLFW_EXPOSE_NATIVE_GLX 1
-#include <GLFW/glfw3native.h>
-#undef Success
-#undef Always
-#undef None
-#endif
 
-using namespace sgd;
+#include <dawn/dawn_proc.h>
+#include <dawn/native/DawnNative.h>
+#include <webgpu/webgpu_cpp.h>
 
-GLFWwindow* window;
+// ***** OPTIONS *****
 
-wgpu::Device device;
-wgpu::Surface surface;
-wgpu::Texture renderTarget;
+wgpu::PresentMode wgpuPresentMode = wgpu::PresentMode::Fifo;			// only mnode guaranteed to be supported on Vulkan?
+//wgpu::PresentMode wgpuPresentMode = wgpu::PresentMode::FifoRelaxed;	// Suffers input lag
+//wgpu::PresentMode wgpuPresentMode = wgpu::PresentMode::Mailbox;		// skips frames and thrashes cpu/gpu.
+//wgpu::PresentMode wgpuPresentMode = wgpu::PresentMode::Immediate;		// no input lag but no vsync + tearing.
 
-void render() {
+// *****
 
-	drawHelloTriangle(device, renderTarget);
+GLFWwindow* glfwWindow;
 
-	struct wgpu::SurfaceTexture surfaceTexture {};
-	surface.GetCurrentTexture(&surfaceTexture);
-	assert(surfaceTexture.status == wgpu::SurfaceGetCurrentTextureStatus::Success);
+wgpu::Instance wgpuInstance;
 
-	copyTexture(device, renderTarget, surfaceTexture.texture);
+wgpu::Adapter wgpuAdapter;
+wgpu::Device wgpuDevice;
 
-	surface.Present();
+wgpu::TextureFormat wgpuSurfaceFormat;
+wgpu::Surface wgpuSurface;
+
+wgpu::RenderPipeline wgpuPipeline;
+
+wgpu::Color wgpuClearColor;
+
+wgpu::Future wgpuWorkDone;
+
+auto shaderSource = R"(
+
+@vertex fn vertexMain(@builtin(vertex_index) vertexId: u32) -> @builtin(position) vec4f {
+    const verts = array<vec2f, 3>(vec2f( 0.0,  0.5), vec2f(-0.5, -0.5), vec2f( 0.5, -0.5));
+
+    return vec4f(verts[vertexId], 0.0, 1.0);
 }
+
+@fragment fn fragmentMain() -> @location(0) vec4f {
+
+    return vec4f(1.0, 0.0, 0.0, 1.0);
+}
+
+)";
 
 int main() {
 
 	uint32_t width = 640;
 	uint32_t height = 480;
 
-	if (!glfwInit()) SGD_ABORT();
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-#if SGD_OS_MACOS
-	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_FALSE);
-#endif
+	// Create window
+	{
+		if (!glfwInit()) std::abort();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindow = glfwCreateWindow((int)width, (int)height, "VSync test", nullptr, nullptr);
+		assert(glfwWindow);
 
-	window = glfwCreateWindow((int)width, (int)height, "Dawn test", nullptr, nullptr);
+		glfwSetKeyCallback(glfwWindow, [](GLFWwindow*, int key, int scancode, int action, int mods) {
+			if(action!=GLFW_PRESS) return;
+			printf("KeyHit key=%i\n", key);
+			fflush(stdout);
+			wgpuClearColor.r = 1 - wgpuClearColor.r;
+		});
+	}
 
-	device = createWGPUDevice({});
-	surface = createWGPUSurface(device, window);
+	// Create instance
+	{
+		dawnProcSetProcs(&dawn::native::GetProcs());
+		wgpu::InstanceDescriptor desc{};
+		desc.features.timedWaitAnyEnable = true;
+		wgpuInstance = wgpu::CreateInstance(&desc);
+		assert(wgpuInstance);
+	}
 
-	static auto configureSurface = [=](CVec2u size) {
+	// Create adapter and device
+	{
+		wgpu::RequestAdapterOptions adapterOpts{};
+
+		(void)wgpuInstance.RequestAdapter( //
+			&adapterOpts, wgpu::CallbackMode::AllowProcessEvents,
+			[](wgpu::RequestAdapterStatus status, wgpu::Adapter adapter, const char* message) {
+				//
+				assert(status == wgpu::RequestAdapterStatus::Success);
+				wgpuAdapter = std::move(adapter);
+
+				wgpu::DeviceDescriptor devDesc{};
+
+				(void)wgpuAdapter.RequestDevice( //
+					&devDesc, wgpu::CallbackMode::AllowProcessEvents,
+					[](wgpu::RequestDeviceStatus status, wgpu::Device device, const char* message) {
+						//
+						assert(status == wgpu::RequestDeviceStatus::Success);
+						wgpuDevice = std::move(device);
+					});
+				wgpuInstance.ProcessEvents();
+			});
+		wgpuInstance.ProcessEvents();
+		assert(wgpuDevice);
+	}
+
+	// Create and configure surface
+	{
+		wgpu::SurfaceDescriptorFromWindowsHWND nativeDesc{};
+		nativeDesc.hwnd = glfwGetWin32Window(glfwWindow);
+		nativeDesc.hinstance = GetModuleHandle(nullptr);
+		wgpu::SurfaceDescriptor surfaceDesc{};
+		surfaceDesc.nextInChain = &nativeDesc;
+		wgpuSurface = wgpuInstance.CreateSurface(&surfaceDesc);
+		assert(wgpuSurface);
+
+		wgpu::SurfaceCapabilities surfCaps{};
+		if (!wgpuSurface.GetCapabilities(wgpuDevice.GetAdapter(), &surfCaps) || !surfCaps.formatCount) {
+			abort();
+		}
+		wgpuSurfaceFormat = surfCaps.formats[0];
+
 		wgpu::SurfaceConfiguration config{};
-		config.device = device;
+		config.device = wgpuDevice;
 		config.width = width;
 		config.height = height;
-		config.format = surface.GetPreferredFormat(device.GetAdapter());
+		config.format = wgpuSurfaceFormat;
 		config.usage = wgpu::TextureUsage::RenderAttachment;
-		config.presentMode = wgpu::PresentMode::Fifo;
+		config.presentMode = wgpuPresentMode;
 		config.viewFormatCount = 0;
 		config.viewFormats = nullptr;
 		config.alphaMode = wgpu::CompositeAlphaMode::Opaque;
-		surface.Configure(&config);
-	};
-	configureSurface(Vec2u(width,height));
-
-	static auto resizeRenderTarget = [=](CVec2u size) {
-		wgpu::TextureDescriptor desc{};
-		desc.size = {size.x,size.y,1};
-		desc.format = wgpu::TextureFormat::RGBA16Float;
-		desc.usage = wgpu::TextureUsage::RenderAttachment | wgpu::TextureUsage::TextureBinding;
-		renderTarget = device.CreateTexture(&desc);
-	};
-	resizeRenderTarget(Vec2u(width,height));
-
-	glfwSetWindowSizeCallback(window, [](GLFWwindow*, int x, int y) {
-		configureSurface(Vec2u(x,y));
-		resizeRenderTarget(Vec2u(x,y));
-	});
-
-	for (;;) {
-		glfwPollEvents();
-		if (glfwWindowShouldClose(window)) std::exit(0);
-
-		render();
+		wgpuSurface.Configure(&config);
 	}
+
+	// Create pipeline
+	{
+		// Compile shader
+		wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
+		wgslDesc.sType = wgpu::SType::ShaderModuleWGSLDescriptor;
+		wgslDesc.code = shaderSource;
+		wgpu::ShaderModuleDescriptor desc{};
+		desc.nextInChain = (wgpu::ChainedStruct*)&wgslDesc;
+		auto shaderModule = wgpuDevice.CreateShaderModule(&desc);
+		assert(shaderModule);
+
+		wgpu::RenderPipelineDescriptor rpDesc{};
+
+		// Fragment state
+		wgpu::ColorTargetState colorTarget;
+		colorTarget.format = wgpuSurfaceFormat;
+		wgpu::FragmentState fragment;
+		fragment.module = shaderModule;
+		fragment.entryPoint = "fragmentMain";
+		fragment.targetCount = 1;
+		fragment.targets = &colorTarget;
+		rpDesc.fragment = &fragment;
+
+		// Vertex state
+		rpDesc.vertex.module = shaderModule;
+		rpDesc.vertex.entryPoint = "vertexMain";
+
+		// Other state
+		wgpu::PipelineLayoutDescriptor plDesc;
+		plDesc.bindGroupLayoutCount = 0;
+		rpDesc.layout = wgpuDevice.CreatePipelineLayout(&plDesc);
+
+		wgpuPipeline = wgpuDevice.CreateRenderPipeline(&rpDesc);
+	}
+
+	// Render loop!
+	for (;;) {
+
+		glfwPollEvents();
+		if (glfwWindowShouldClose(glfwWindow)) break;
+
+		// Begin rendering
+		auto encoder = wgpuDevice.CreateCommandEncoder();
+
+		// Render directly to swapchain
+		struct wgpu::SurfaceTexture surfaceTexture {};
+		wgpuSurface.GetCurrentTexture(&surfaceTexture);
+		assert(surfaceTexture.status == wgpu::SurfaceGetCurrentTextureStatus::Success);
+		auto renderTarget = surfaceTexture.texture;
+
+		// Create renderpass
+		wgpu::RenderPassColorAttachment colorAttachment{};
+		colorAttachment.view = renderTarget.CreateView();
+		colorAttachment.clearValue = wgpuClearColor;//{1.0f, 0.7f, 0.0f, 0.0f};
+		colorAttachment.loadOp = wgpu::LoadOp::Clear;
+		colorAttachment.storeOp = wgpu::StoreOp::Store;
+		wgpu::RenderPassDescriptor desc{};
+		desc.colorAttachmentCount = 1;
+		desc.colorAttachments = &colorAttachment;
+		auto renderPass = encoder.BeginRenderPass(&desc);
+
+		// Render triangle
+		renderPass.SetPipeline(wgpuPipeline);
+		renderPass.Draw(3, 1, 0, 0);
+		renderPass.End();
+
+		// Is this in the right place?
+		if (vsyncEnabled) {
+			// Wait for previous work to finished before submitting any new work.
+			if(wgpuWorkDone.id) {
+				// wait 5 seconds
+				wgpuInstance.WaitAny(wgpuWorkDone, 5000000000);
+				wgpuWorkDone.id = 0;
+			}
+			wgpuWorkDone = wgpuDevice.GetQueue().OnSubmittedWorkDone(
+				wgpu::CallbackMode::WaitAnyOnly, [](wgpu::QueueWorkDoneStatus status) {});
+		}
+
+		// End rendering (begin on GPU!)
+		auto commands = encoder.Finish();
+		wgpuDevice.GetQueue().Submit(1, &commands);
+
+		// SwapBuffers
+		wgpuSurface.Present();
+
+		// Is this the best place for this?
+		wgpuDevice.GetAdapter().GetInstance().ProcessEvents();
+	}
+
+	// Does this destroy *everything*?
+	wgpuDevice.Destroy();
+
+	glfwDestroyWindow(glfwWindow);
+	glfwTerminate();
+	return 0;
 }
---
+++ dawn/tests/metal_tests.mm
@@ -0,0 +1,297 @@
+/*
+
+Minimal example of a weird problem that appears to involves textures. The app will intermittently freeze at start, with either the dreaded
+'magenta window of death' or the first frame correctly displayed.
+
+After that, it will either lock up forever and you'll have to reboot, or it will recover after 10-20 seconds, but possibly with one frame stuck at
+magenta. Sometimes it appears to recovers fully.
+
+It's pretty hard to get to occur, you may have to run it many times. Running with 'lldb' from the command line seems to work best, although I have
+had it die on me from plain shell, xcode, even CLion. You can also try only running the main loop 30 times or so (see comment) and calling
+the app repeatedly from a bash/python script.
+
+Changing the length of the array seems to have an effect too, setting it to longer values seems make the problem occur more often but that may just be
+my imagination!
+
+I've seen the problem occur with plain 2D textures too, but that seemed to be much harder to reproduce so I've left it as is.
+
+*/
+
+#include <cassert>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_COCOA
+#include "GLFW/glfw3native.h"
+
+#import <QuartzCore/CAMetalLayer.h>
+
+#include <dawn/dawn_proc.h>
+#include <dawn/native/DawnNative.h>
+#include <webgpu/webgpu_cpp.h>
+
+// ***** OPTIONS *****
+
+uint32_t width = 640;
+uint32_t height = 480;
+
+// Set to false to disable bindgroup - program works normally
+// Set to true to enable bindgroup - program displays a magenta view. In bigger apps this may crash too.
+bool bindGroupEnabled = true;
+
+// *****
+
+GLFWwindow* glfwWindow;
+
+wgpu::Instance wgpuInstance;
+
+wgpu::Adapter wgpuAdapter;
+wgpu::Device wgpuDevice;
+
+wgpu::TextureFormat wgpuSurfaceFormat;
+wgpu::Surface wgpuSurface;
+
+wgpu::BindGroupLayout wgpuBindGroupLayout;
+wgpu::BindGroup wgpuBindGroup;
+
+wgpu::RenderPipeline wgpuPipeline;
+
+wgpu::Future wgpuWorkDone;
+
+auto shaderSource = R"(
+
+@group(0) @binding(0) var texture: texture_2d_array<f32>;
+
+@vertex fn vertexMain(@builtin(vertex_index) vertexId: u32) -> @builtin(position) vec4f {
+    const verts = array<vec2f, 3>(vec2f( 0.0,  0.5), vec2f(-0.5, -0.5), vec2f( 0.5, -0.5));
+
+    return vec4f(verts[vertexId], 0.0, 1.0);
+}
+
+@fragment fn fragmentMain() -> @location(0) vec4f {
+
+    return vec4f(1.0, 0.0, 0.0, 1.0);
+}
+
+)";
+
+void createWindow() {
+
+	if (!glfwInit()) std::abort();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindow = glfwCreateWindow((int)width, (int)height, "Metal Test", nullptr, nullptr);
+	assert(glfwWindow);
+}
+
+void createInstance() {
+	dawnProcSetProcs(&dawn::native::GetProcs());
+	wgpu::InstanceDescriptor desc{};
+	desc.features.timedWaitAnyEnable = true;
+	wgpuInstance = wgpu::CreateInstance(&desc);
+	assert(wgpuInstance);
+}
+
+void deviceErrorCallback(WGPUErrorType type, const char* message,void*) {
+	printf("### Dawn error: %s\n",message);
+	fflush(stdout);
+	std::exit(1);
+};
+
+void createDevice()	{
+	wgpu::RequestAdapterOptions adapterOpts{};
+
+	(void)wgpuInstance.RequestAdapter( //
+		&adapterOpts, wgpu::CallbackMode::AllowProcessEvents,
+		[](wgpu::RequestAdapterStatus status, wgpu::Adapter adapter, const char* message) {
+			//
+			assert(status == wgpu::RequestAdapterStatus::Success);
+			wgpuAdapter = std::move(adapter);
+
+			wgpu::DeviceDescriptor devDesc{};
+            devDesc.uncapturedErrorCallbackInfo.callback = &deviceErrorCallback;
+
+			(void)wgpuAdapter.RequestDevice( //
+				&devDesc, wgpu::CallbackMode::AllowProcessEvents,
+				[](wgpu::RequestDeviceStatus status, wgpu::Device device, const char* message) {
+					//
+					assert(status == wgpu::RequestDeviceStatus::Success);
+
+
+					wgpuDevice = std::move(device);
+				});
+			wgpuInstance.ProcessEvents();
+		});
+	wgpuInstance.ProcessEvents();
+	assert(wgpuDevice);
+}
+
+void createSurface(){
+	NSWindow* nsWindow = glfwGetCocoaWindow(glfwWindow);
+	NSView* view = [nsWindow contentView];
+
+	[view setWantsLayer:YES];
+	[view setLayer:[CAMetalLayer layer]];
+
+	// Use retina if the window was created with retina support.
+	[[view layer] setContentsScale:[nsWindow backingScaleFactor]];
+
+	wgpu::SurfaceDescriptorFromMetalLayer nativeDesc{};
+	nativeDesc.layer = [view layer];
+
+	wgpu::SurfaceDescriptor surfaceDesc{};
+	surfaceDesc.nextInChain = &nativeDesc;
+
+	wgpuSurface = wgpuInstance.CreateSurface(&surfaceDesc);
+	assert(wgpuSurface);
+
+	wgpu::SurfaceCapabilities surfCaps{};
+	if (!wgpuSurface.GetCapabilities(wgpuDevice.GetAdapter(), &surfCaps) || !surfCaps.formatCount) {
+		abort();
+	}
+	wgpuSurfaceFormat = surfCaps.formats[0];
+
+	wgpu::SurfaceConfiguration config{};
+	config.device = wgpuDevice;
+	config.width = width;
+	config.height = height;
+	config.format = wgpuSurfaceFormat;
+	config.usage = wgpu::TextureUsage::RenderAttachment;
+	config.presentMode = wgpu::PresentMode::Fifo;
+	config.alphaMode = wgpu::CompositeAlphaMode::Opaque;
+	wgpuSurface.Configure(&config);
+}
+
+void createBindGroup() {	// Create BindGroupLayout and BindGroup
+
+	// Create layout
+	wgpu::BindGroupLayoutEntry bglEntries[1]{};
+	bglEntries[0].binding = 0;
+	bglEntries[0].visibility = wgpu::ShaderStage::Fragment;
+	bglEntries[0].texture.viewDimension = wgpu::TextureViewDimension::e2DArray;
+	bglEntries[0].texture.sampleType=wgpu::TextureSampleType::Float;
+	wgpu::BindGroupLayoutDescriptor bglDesc{};
+	bglDesc.entries=bglEntries;
+	bglDesc.entryCount=std::size(bglEntries);
+	wgpuBindGroupLayout = wgpuDevice.CreateBindGroupLayout(&bglDesc);
+
+	// Create texture
+	wgpu::TextureDescriptor texDesc{};
+	texDesc.size = {1024,1024,16};
+	texDesc.format = wgpu::TextureFormat::RGBA8Unorm;
+	texDesc.usage = wgpu::TextureUsage::TextureBinding | wgpu::TextureUsage::CopyDst;
+	auto wgpuTexture = wgpuDevice.CreateTexture(&texDesc);
+
+	wgpu::TextureViewDescriptor tvDesc{};
+	tvDesc.dimension = wgpu::TextureViewDimension::e2DArray;
+
+	// Create bind group
+	wgpu::BindGroupEntry bgEntries[1];
+	bgEntries[0].binding = 0;
+	bgEntries[0].textureView = wgpuTexture.CreateView(&tvDesc);
+
+	wgpu::BindGroupDescriptor bgDesc{};
+	bgDesc.layout = wgpuBindGroupLayout;
+	bgDesc.entries = bgEntries;
+	bgDesc.entryCount = std::size(bgEntries);
+	wgpuBindGroup = wgpuDevice.CreateBindGroup(&bgDesc);
+}
+
+// Create pipeline
+void createPipeline()	{
+	// Compile shader
+	wgpu::ShaderModuleWGSLDescriptor wgslDesc{};
+	wgslDesc.sType = wgpu::SType::ShaderModuleWGSLDescriptor;
+	wgslDesc.code = shaderSource;
+	wgpu::ShaderModuleDescriptor desc{};
+	desc.nextInChain = (wgpu::ChainedStruct*)&wgslDesc;
+	auto shaderModule = wgpuDevice.CreateShaderModule(&desc);
+	assert(shaderModule);
+
+	wgpu::RenderPipelineDescriptor rpDesc{};
+
+	// Fragment state
+	wgpu::ColorTargetState colorTarget{};
+	colorTarget.format = wgpuSurfaceFormat;
+	wgpu::FragmentState fragment{};
+	fragment.module = shaderModule;
+	fragment.entryPoint = "fragmentMain";
+	fragment.targetCount = 1;
+	fragment.targets = &colorTarget;
+	rpDesc.fragment = &fragment;
+
+	// Vertex state
+	rpDesc.vertex.module = shaderModule;
+	rpDesc.vertex.entryPoint = "vertexMain";
+
+	// Bind group layouts
+	wgpu::PipelineLayoutDescriptor plDesc{};
+	plDesc.bindGroupLayouts = &wgpuBindGroupLayout;
+	plDesc.bindGroupLayoutCount = (int)bindGroupEnabled;
+	rpDesc.layout = wgpuDevice.CreatePipelineLayout(&plDesc);
+
+	wgpuPipeline = wgpuDevice.CreateRenderPipeline(&rpDesc);
+}
+
+int main() {
+
+    createWindow();
+
+    createInstance();
+
+    createDevice();
+
+    createSurface();
+
+    createBindGroup();
+
+    createPipeline();
+
+	// Render loop!
+	for (;;) {
+//	for(int i=0;i<30;++i) {
+
+        // Note: without this, there's no vsync on MacOS
+		glfwPollEvents();
+		if (glfwWindowShouldClose(glfwWindow)) break;
+
+		// Begin rendering
+		auto encoder = wgpuDevice.CreateCommandEncoder();
+
+		// Render directly to swapchain
+		struct wgpu::SurfaceTexture surfaceTexture {};
+		wgpuSurface.GetCurrentTexture(&surfaceTexture);
+		assert(surfaceTexture.status == wgpu::SurfaceGetCurrentTextureStatus::Success);
+		auto renderTarget = surfaceTexture.texture;
+
+		// Create renderpass
+		wgpu::RenderPassColorAttachment colorAttachment{};
+		colorAttachment.view = renderTarget.CreateView();
+		colorAttachment.clearValue = {1.0f, 0.7f, 0.0f, 0.0f};
+		colorAttachment.loadOp = wgpu::LoadOp::Clear;
+		colorAttachment.storeOp = wgpu::StoreOp::Store;
+		wgpu::RenderPassDescriptor desc{};
+		desc.colorAttachmentCount = 1;
+		desc.colorAttachments = &colorAttachment;
+		auto renderPass = encoder.BeginRenderPass(&desc);
+
+		renderPass.SetBindGroup(0, wgpuBindGroup);
+
+		// Render triangle
+		renderPass.SetPipeline(wgpuPipeline);
+		renderPass.Draw(3, 1, 0, 0);
+		renderPass.End();
+
+		auto commands = encoder.Finish();
+		wgpuDevice.GetQueue().Submit(1, &commands);
+
+		// SwapBuffers
+		wgpuSurface.Present();
+
+		wgpuDevice.GetAdapter().GetInstance().ProcessEvents();
+	}
+
+	wgpuDevice.Destroy();
+
+	glfwDestroyWindow(glfwWindow);
+	glfwTerminate();
+	return 0;
+}
